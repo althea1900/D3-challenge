eb012a43d4e33f861b65c8f44b65a086a92224d7
Althea McMillian <amcmilli@richmond.edu>
2020-09-11 21:59:29 -0400

    work on axes labelstweaks

--- d3_data_journalism/assets/js/app.js
+++ d3_data_journalism/assets/js/app.js
@@ -12,7 +12,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".scatter")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
